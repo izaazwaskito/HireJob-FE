4a1d7f215373a948ad0b6b9e182005b3595ce5c5
Izaaz Waskito <116268166+izaazwaskito@users.noreply.github.com>
2023-08-31 10:51:55 +0700

    async

--- pages/_document.js
+++ pages/_document.js
@@ -23,13 +23,13 @@ export default function Document() {
           src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
           integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
           crossOrigin="anonymous"
-          async
+          defer
         />
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
           crossOrigin="anonymous"
-          async
+          defer
         />
         {/* <script src="sweetalert2.all.min.js"></script> */}
       </body>
