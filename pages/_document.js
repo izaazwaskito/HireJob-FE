6acfbb908dca4962da0cfbcf2289fb3bb3ac190c
Izaaz Waskito <116268166+izaazwaskito@users.noreply.github.com>
2023-09-11 19:17:35 +0700

    add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <title>Peworld</title>
         <meta charet="utf-8" />
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
